188066d55ecdecd66ce0bbd8c5b0e0f4cf92689d
Van Tuan Tran <4468525+huntertran@users.noreply.github.com>
2020-03-12 15:55:18 -0400

    call the sort method

--- patterns/singleton/App.java
+++ patterns/singleton/App.java
@@ -3,5 +3,8 @@ package patterns.singleton;
 public class App {
     public static void main(String[] args) {
         var sortAlgorithms = Factory.getInstance().getAllSortAlgorithm();
+        for (ISort iSort : sortAlgorithms) {
+            iSort.sort();
+        }
     }
 }
